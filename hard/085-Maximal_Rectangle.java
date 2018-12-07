fa9fecd522f69cbba28a856086b56785a12ad2fe
chenliangxs <chenliangxs@gmail.com>
2018-12-07 00:24:21 -0800

    clean

--- hard/085-Maximal_Rectangle.java
+++ hard/085-Maximal_Rectangle.java
@@ -1,58 +1,55 @@
 
-Given a 2D binary matrix filled with 0's and 1's, find the largest rectangle containing only 1's and return its area.
+// Given a 2D binary matrix filled with 0's and 1's, find the largest rectangle containing only 1's and return its area.
+//
+// Example:
+//
+// Input:
+// [
+//   ["1","0","1","0","0"],
+//   ["1","0","1","1","1"],
+//   ["1","1","1","1","1"],
+//   ["1","0","0","1","0"]
+// ]
+// Output: 6
+//
+// ================================================
 
-Example:
 
-Input:
-[
-  ["1","0","1","0","0"],
-  ["1","0","1","1","1"],
-  ["1","1","1","1","1"],
-  ["1","0","0","1","0"]
-]
-Output: 6
-
-================================================
 public int maximalRectangle(char[][] matrix) {
-        if(matrix.length == 0 || matrix[0].length == 0) return 0;
-        int[] heights = new int[matrix[0].length];
-        int max = 0;
-        for(int i=0; i<matrix.length; i++){
-            for(int j = 0; j<matrix[0].length; j++){
-                if(matrix[i][j] == '1'){
-                    heights[j] = heights[j]+1;
-                }else{
-                    heights[j] = 0;
-                }
-            }
-            max = Math.max(max, maxRectangle(heights));
-        }
-        return max;
-    }
-    public int maxRectangle(int[] array){
-        if(array.length == 0) return 0;
-        int max = 0;
-        Deque<Integer> stack = new ArrayDeque<>();
-        for(int i=0; i<=array.length; i++){
-            int height = (i==array.length)?0:array[i];
-            if(stack.isEmpty() || array[stack.peekFirst()] < height){
-                stack.offerFirst(i);
+    if(matrix.length == 0 || matrix[0].length == 0) return 0;
+    int[] heights = new int[matrix[0].length];
+    int max = 0;
+    for(int i=0; i<matrix.length; i++){
+        for(int j = 0; j<matrix[0].length; j++){
+            if(matrix[i][j] == '1'){
+                heights[j] = heights[j]+1;
             }else{
-                while(!stack.isEmpty() && array[stack.peekFirst()] >= height){
-                    int index = stack.pollFirst();
-                    if(stack.isEmpty()){
-                        max = Math.max(max, array[index] * i);
-                    }else{
-                        max = Math.max(max, array[index] * (i - stack.peekFirst() - 1));
-                    }
-                }
-                stack.offerFirst(i);
+                heights[j] = 0;
             }
         }
-        return max;
+        max = Math.max(max, maxRectangle(heights));
     }
-
-=====================================================
-matrix[i][j] = 0 dp[i][j] = 0;
-
-dp[i][j]
+    return max;
+}
+public int maxRectangle(int[] array){
+    if(array.length == 0) return 0;
+    int max = 0;
+    Deque<Integer> stack = new ArrayDeque<>();
+    for(int i=0; i<=array.length; i++){
+        int height = (i==array.length)?0:array[i];
+        if(stack.isEmpty() || array[stack.peekFirst()] < height){
+            stack.offerFirst(i);
+        }else{
+            while(!stack.isEmpty() && array[stack.peekFirst()] >= height){
+                int index = stack.pollFirst();
+                if(stack.isEmpty()){
+                    max = Math.max(max, array[index] * i);
+                }else{
+                    max = Math.max(max, array[index] * (i - stack.peekFirst() - 1));
+                }
+            }
+            stack.offerFirst(i);
+        }
+    }
+    return max;
+}
--- hard/097-Interleaving_String.java
+++ hard/097-Interleaving_String.java
@@ -1,45 +1,45 @@
-s1: i
-
-s2: j
-
-s3: i+j - 1
-
-dp[i][j]: if(s[i] == s3[i+j-1]) dp[i-1][j]
-	if s2[j] == s3[i+j-1] dp[i][j-1]
-
-else false; 
-
-Given s1, s2, s3, find whether s3 is formed by the interleaving of s1 and s2.
-
-Example 1:
-
-Input: s1 = "aabcc", s2 = "dbbca", s3 = "aadbbcbcac"
-Output: true
-Example 2:
-
-Input: s1 = "aabcc", s2 = "dbbca", s3 = "aadbbbaccc"
-Output: false
-
+// s1: i
+//
+// s2: j
+//
+// s3: i+j - 1
+//
+// dp[i][j]: if(s[i] == s3[i+j-1]) dp[i-1][j]
+// 	if s2[j] == s3[i+j-1] dp[i][j-1]
+//
+// else false;
+//
+// Given s1, s2, s3, find whether s3 is formed by the interleaving of s1 and s2.
+//
+// Example 1:
+//
+// Input: s1 = "aabcc", s2 = "dbbca", s3 = "aadbbcbcac"
+// Output: true
+// Example 2:
+//
+// Input: s1 = "aabcc", s2 = "dbbca", s3 = "aadbbbaccc"
+// Output: false
+//=======================================
 
 public boolean isInterleave(String s1, String s2, String s3) {
-        int m = s1.length();
-        int n = s2.length();
-        if(m + n != s3.length()) return false;
-        boolean[][] dp = new boolean[m+1][n+1];
-        dp[0][0] = true;
-        for(int i=0; i<=m; i++){
-            for(int j=0; j<=n; j++){
-                if(i==0 || j==0){
-                    if(i==0&&j==0) continue;
-                    else if(i==0){
-                        dp[i][j] = (s2.charAt(j-1) == s3.charAt(j-1) && dp[i][j-1]);
-                    }else{
-                        dp[i][j] = (s1.charAt(i-1) == s3.charAt(i-1) && dp[i-1][j]);
-                    }
-                }else{
-                    dp[i][j] = (s1.charAt(i-1) == s3.charAt(i+j-1) && dp[i-1][j]) || (s2.charAt(j-1) == s3.charAt(i+j-1) && dp[i][j-1]);
-                }
-            }
-        }
-        return dp[m][n];
-    }
+	int m = s1.length();
+	int n = s2.length();
+	if(m + n != s3.length()) return false;
+	boolean[][] dp = new boolean[m+1][n+1];
+	dp[0][0] = true;
+	for(int i=0; i<=m; i++){
+		for(int j=0; j<=n; j++){
+			if(i==0 || j==0){
+				if(i==0&&j==0) continue;
+				else if(i==0){
+					dp[i][j] = (s2.charAt(j-1) == s3.charAt(j-1) && dp[i][j-1]);
+				}else{
+					dp[i][j] = (s1.charAt(i-1) == s3.charAt(i-1) && dp[i-1][j]);
+				}
+			}else{
+				dp[i][j] = (s1.charAt(i-1) == s3.charAt(i+j-1) && dp[i-1][j]) || (s2.charAt(j-1) == s3.charAt(i+j-1) && dp[i][j-1]);
+			}
+		}
+	}
+	return dp[m][n];
+}
